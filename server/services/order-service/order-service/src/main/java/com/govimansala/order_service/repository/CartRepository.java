5835924b0a48ae0f7751ef03cd8dbf11582d7cfc
Madara Meegama <madarameegama7@gmail.com>
2025-07-22 22:32:29 +0530

    Order service- cart function

--- server/services/order-service/order-service/src/main/java/com/govimansala/order_service/repository/CartRepository.java
+++ server/services/order-service/order-service/src/main/java/com/govimansala/order_service/repository/CartRepository.java
@@ -4,6 +4,8 @@ import com.govimansala.order_service.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.List;
+
 public interface CartRepository extends JpaRepository<Cart,Integer>{
-    Optional<Cart> findByUserId(int userId);
+    List<Cart> findByUserId(int userId);
 }
