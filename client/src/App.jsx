9fc9bbeb13a6bfe3ff3b1cd1de04dde8d780758c
Madara Meegama <madarameegama7@gmail.com>
2025-10-19 03:19:07 +0530

    Changed app.jsx with user dashboard view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -50,6 +50,7 @@ import VendorDetails from "./pages/Admin/VendorDetails";
 import DriverDetails from "./pages/Admin/DriverDetails";
 import QADetails from "./pages/Admin/QADetails";
 import FarmerDetails from "./pages/Admin/FarmerDetails";
+import UserDetails from "./pages/Admin/UserDetails";
 
 function App() {
   return (
@@ -144,6 +145,7 @@ function App() {
           <Route path="DriverDetails" element={<DriverDetails />} />
           <Route path="QADetails" element={<QADetails />} />
           <Route path="FarmerDetails" element={<FarmerDetails />} />
+          <Route path="UserDetails" element={<UserDetails />} />
         </Route>
         
         {/* DRIVER ROUTES */}
