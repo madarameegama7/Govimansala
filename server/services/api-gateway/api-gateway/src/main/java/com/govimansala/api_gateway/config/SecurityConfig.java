3e31f161bd661fbff75d12be822e9638165f25b1
Madara Meegama <madarameegama7@gmail.com>
2025-10-20 02:30:27 +0530

    Added new routes in product service

--- server/services/api-gateway/api-gateway/src/main/java/com/govimansala/api_gateway/config/SecurityConfig.java
+++ server/services/api-gateway/api-gateway/src/main/java/com/govimansala/api_gateway/config/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/api/auth/**","/api/product/**","/api/order/**","/api/admin/farmers/**","/api/admin/buyers/**","/api/admin/vendors/**","/api/admin/drivers/**","/api/vendors/**").permitAll()
+                        .pathMatchers("/api/auth/**","/api/product/**","/api/order/**","/api/admin/farmers/**","/api/admin/buyers/**","/api/admin/vendors/**","/api/admin/drivers/**","/api/vendors/**","/api/product/farmer_product/**").permitAll()
 
 
                         .anyExchange().authenticated()
