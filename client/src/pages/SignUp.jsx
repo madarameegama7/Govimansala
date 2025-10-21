953ed989fbace8ffdb853e43acdd70334c430346
Madara Meegama <madarameegama7@gmail.com>
2025-10-21 05:56:51 +0530

    Mobile app register

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -28,6 +28,10 @@ function SignUp() {
     );
   };
 
+  const isValidPhoneNumber = (phoneNumber) => {
+    return /^\+?[1-9]\d{1,14}$/.test(phoneNumber);
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -46,6 +50,15 @@ function SignUp() {
         icon: "error",
         title: "Weak Password",
         text: "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character",
+        confirmButtonColor: "#d33",
+      });
+      return;
+    }
+    if (!isValidPhoneNumber(mobile)) {
+      Swal.fire({
+        icon: "error",        
+        title: "Invalid Mobile Number",
+        text: "Please enter a valid mobile number",
         confirmButtonColor: "#d33",
       });
       return;
